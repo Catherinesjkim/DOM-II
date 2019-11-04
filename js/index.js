299f45374df8cdceed51b950f535c8b45d8ba2d1
Catherine Kim <catherinesjkim@gmail.com>
2019-11-04 07:24:25 -0800

    Add Updated JS File & Bug Fix Because: - Rearranged order of the event listeners according to the design order of the HTML
    This commit:
    - Fixed a bug by changing from blue to blur. Now when the name input is focused, the background color changes from blue to light green
    - Added a change button text so that when a user clicks on the sign me up buttons, the text changes to Almost there
    - Took out the radio event since it wasn't working.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 // Your code goes here
+
+// ON DOCUMENT LOAD AN ALERT WILL APPEAR TELLLING THE USER THE 9 OTHER THINGS THEY CAN DO WITH THE SITE
+window.addEventListener("load", function(event) {
+  alert("All resources finished loading! Ready for some FUN? Try scrolling the page, try hovering over the nav logo heading, try double clicking the nav links, try dragging the `Fun Bus` banner image, try pressing a key on your keyboard...but wait...where did the nav bar go?...that's ok, just hover over anywhere inside it to bring it back! Focus the name field in the form to see its background color change and leave its focus, aka blur event, to remove the background color. Try re-sizing the window and see what happens. Click on the `Fun In the Sun packages` div and look in the console to see that you clicked it...do the same with the Sign Me Up button inside that div and notice that it console.logs its own message without console.logging that the div was clicked...that's the power of the stopPropagation method...that's it!");
+});
+
 const navLogo = document.querySelector("body");
 navLogo.addEventListener("mouseover", (event) => {
   event.target.style.color = "purple";
@@ -23,40 +29,25 @@ nav.addEventListener('click', (event) => {
   console.log("clicked on navlink");
 });
 
-const signUpButton1 = document.querySelector(".btn");
-const signUpButton1Div = document.querySelector(".destination");
-
-// ADDED CONSOLE LOG FOR WHEN FUN IN THE SUN DIV IS CLICKED
-signUpButton1Div.addEventListener("click", (event) => {
-  console.log("div-clicked"); // Will only show on the inspect console 
-})
-console.log(signUpButton1Div);
-
-// STOPPING THE BUBBLING OF DIV CLICKED CONSOLE LOG
-signUpButton1.addEventListener('click', (event) => {
-  console.log("signup 1 clicked"); // Will only show on the inspect console 
-  event.stopPropagation(); 
-})
-
-const body = document.querySelector('body');
-const header = document.querySelector('header');
+const body = document.querySelector("body");
+const header = document.querySelector("header");
 // PRESS A KEYBOARD KEY AND THE HEADER WILL DISAPPEAR FROM VISION
-body.addEventListener("keydown", (event) => header.style.opacity = "0");
+body.addEventListener("keydown", event => (header.style.opacity = "0"));
 
 const navContainer = document.querySelector(".nav-container");
 // MOUSEENTER THE NAVIGATION TO RETURN ITS OPACITY TO 1 TO NEGATE THE ABOVE KEYPRESS OPACITY CHANGE
-navContainer.addEventListener("mouseenter", () => header.style.opacity = "1");
-
-
-//ALERTS OUCH WHENEVER THE WINDOW IS RESIZED
-window.addEventListener('resize', () => {
-  alert('OUCH!')
-});
+navContainer.addEventListener("mouseenter", () => (header.style.opacity = "1"));
 
 // ADDS AN ALERT WHEN THE USER FIRST `SCROLLS` WITH THE MOUSE WHEEL BUT THEN REMOVES THAT EVENT SO THEY CAN ACTUALLY NAVIGATE THE SITE
 body.addEventListener('wheel', function alertMeOnce() {
   alert("YAAAAAY!!!!!");
   body.removeEventListener('wheel', alertMeOnce);
+});
+
+//ALERTS OUCH WHENEVER THE WINDOW IS RESIZED
+window.addEventListener('resize', function alertMeOnce() {
+  alert('OUCH!');
+  window.removeEventListener('resize', alertMeOnce);
 });
 
 const funBusPhotos = document.querySelectorAll("img" );
@@ -67,38 +58,43 @@ funBusPhotos.forEach(image => {
   image.addEventListener("mouseleave", () => event.target.style.transition = 'all 2s');
 });
 
-setTimeout(function (event) {
-  event.target.style.transform = "";
-}, 200);
-false;
-
-// ON DOCUMENT LOAD AN ALERT WILL APPEAR TELLLING THE USER THE 9 OTHER THINGS THEY CAN DO WITH THE SITE
-window.addEventListener("load", function(event) {
-  alert("All resources finished loading! Ready for some FUN? Try scrolling the page, try hovering over the nav logo heading, try double clicking the nav links, try dragging the `Fun Bus` banner image, try pressing a key on your keyboard...but wait...where did the nav bar go?...that's ok, just hover over anywhere inside it to bring it back! Focus the name field in the form to see its background color change and leave its focus, aka blur event, to remove the background color. Try re-sizing the window and see what happens. Click on the `Fun In the Sun packages` div and look in the console to see that you clicked it...do the same with the Sign Me Up button inside that div and notice that it console.logs its own message without console.logging that the div was clicked...that's the power of the stopPropagation method...that's it!");
-});
-
 const sunPackage = document.querySelector("#sun");
 const mountainPackage = document.querySelector("#mountain");
 const islandPackage = document.querySelector("#island");
 
+const signUpButton1 = document.querySelector(".btn");
+const signUpButton1Div = document.querySelector(".destination");
 
-const radios = document.querySelector("#fieldset"); 
+// ADDED CONSOLE LOG FOR WHEN FUN IN THE SUN DIV IS CLICKED
+signUpButton1Div.addEventListener("click", event => {
+  console.log("div-clicked"); // Will only show on the inspect console
+});
+console.log(signUpButton1Div);
 
-// radios.addEventListener('select', radios.style.backgroundColor = "red");
-radios.addEventListener("select", (event) => {
-  event.target.style.backgroundColor = 'red'}, false);
+// STOPPING THE BUBBLING OF DIV CLICKED CONSOLE LOG
+signUpButton1.addEventListener("click", event => {
+  console.log("signup 1 clicked"); // Will only show on the inspect console
+  event.stopPropagation();
+});
+
+const changeBtnText = document.querySelectorAll('.btn');
+changeBtnText.forEach(text => {
+  text.addEventListener('click', () => {
+    text.textContent = "Almost there!";
+  });
+});
 
 const nameInput = document.querySelector("#nameForm");
 const emailForm = document.querySelector("#emailForm");
 const phoneForm = document.querySelector("#phoneForm");
 const specialInstructionsForm = document.querySelector("#specialForm");
 
-// ADDS A BACKGROUND COLOR TO THE INPUTS WHEN IT IS FOCUSED
+// ADDS A BACKGROUND COLOR TO THE NAME INPUT WHEN IT IS FOCUSED
 nameInput.addEventListener("focus", (event) => {
-  nameInput.style.backgroundColor = "orangered"
+  nameInput.style.backgroundColor = "lightgreen"
 });
-// REMOVES THE BACKGROUND COLOR ON THE INPUT WHEN FOCUS IS LEFT...I.E. ON BLUR EVENT
-nameInput.addEventListener('blue', (event) => {
-  nameInput.style.backgroundColor = "inherit"
+// REMOVES THE BACKGROUND COLOR ON THE NAME INPUT WHEN FOCUS IS LEFT...I.E. ON BLUR EVENT
+nameInput.addEventListener("blur", (event) => {
+  event.target.style.backgroundColor = "inherit"
 });
 
